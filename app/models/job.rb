a3320e9f66f10d82a35631fd39bc4561643490e8
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2014-10-01 18:19:53 -0400

    Updates to search and homepage

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,15 +7,23 @@ class Job < ActiveRecord::Base
   acts_as_taggable
 
   def self.search(search)
-    find(:all, :conditions => ['title LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%" ])
+    find(:all, :conditions => ['UPPER(title) LIKE ? OR UPPER(description) LIKE ?', "%#{search.upcase}%", "%#{search.upcase}%" ])
   end
+
   def expired?
     return DateTime.now > self.expires_at
   end
+
   def ustime
     self.expires_at.strftime('%m/%d/%Y %I:%M %p')
   end
+
+  def employer_name
+    Employer.find(employer_id).company_name
+  end
+
   def self.utctime ustime
     DateTime.strptime(ustime, '%m/%d/%Y %I:%M %p').to_s
   end
+
 end
